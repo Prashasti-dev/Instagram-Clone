d0f22304d93d862d6f0aaf1c343038eb1ed8366b
Prashasti Jaiswal <prashasti.2024@gmail.com>
2026-07-23 12:41:41 +0530

    getpostdetails

--- Backend/src/controllers/post.controller.js
+++ Backend/src/controllers/post.controller.js
@@ -48,4 +48,72 @@ let decoded=null
                     post
                 })
 }
-module.exports={createPostController}
+
+async function getPostController(req,res){
+    const token=req.cookies.token
+
+    if(!token){
+        return res.status(401).json({
+            message:"Unauthorized Acess"
+        })
+    }
+
+    let decoded;
+    try{
+    decoded=  jwt.verify(token, process.env,JWT_SECRET)
+    }catch(err){
+    return res.status(401).json({
+        message :"token invalid"
+    })
+ }
+ const userId= decoded.id
+ const post= await postModel.find({
+    user:userId
+ })
+ res.status(200).json({
+    message:"post fetch",posts
+ })
+}
+
+async function getPostDetailsController(req,res){
+    const token=req.cookies.token
+
+     if(!token){
+        return res.status(401).json({
+            message:"Unauthorized Acess"
+        })
+    }
+    let decoded;
+    try{
+            decoded=jwt.verify(token,process.env.JWT_SECRET)
+    }catch(err){
+        return res.status(401).json({
+            message:"Invalid Token"
+        })
+    }
+    const userId=decoded.id
+    const postId=req.params.postId
+    const post=await postModel.findById(postId)
+
+    if(!post){
+        return res.status(404).json({
+            message:"post not found"
+        })
+    }
+    const isValidUser=post.user.toString()===userId
+    if(!isValidUser){
+        return res.status(403).json({
+            message:"fORBIDDEN CONTENT"
+        })
+    }
+
+   res.status(200).json({
+        message:"Post fetched succesfuly",
+        post
+    })
+}
+module.exports={
+    createPostController,
+    getPostController,
+    getPostDetailsController
+}
--- Backend/src/routes/post.routes.js
+++ Backend/src/routes/post.routes.js
@@ -5,12 +5,21 @@ const postController=require('../controllers/post.controller')
 const multer=require('multer')
 const upload=multer({storage:multer.memoryStorage()})
 /**
- * POST/api/posts [protected api]
+ * POST/api/posts [protected api]  jiske pass token nhi hai wo cant acces api
+ * 
  * req.body={caption,image-file}
  */
 //FILE HANDLING
 
 postRouter.post("/",upload.single("image"),postController.createPostController)
 
+// GET/api/posts[protected]
+postRouter.get("/",postController.getPostController)
 
+/**
+ * GET/api/posts/details/:postId
+ * -return a detail about specific post with id
+ * -check whteher the post belongs to user that is requesting 
+ */
+postRouter.get("/details/:postId",postController.getPostDetailsController)
 module.exports=postRouter
